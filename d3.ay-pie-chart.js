24c27a9108c833daa5b9239ed665e89191b69e74
Greg Leaver <greg.leaver@gmail.com>
2013-02-15 16:19:51 -0500

    Allow caller to specify naming function used to create labels.

--- d3.ay-pie-chart.js
+++ d3.ay-pie-chart.js
@@ -26,7 +26,8 @@ define([], function(){
           percentage: true,
           value: false,
           label_margin: 10,
-          group_data: 0
+          group_data: 0,
+          name: function(d){ return d.name; }
         },
         donut,
         arc,
@@ -146,7 +147,7 @@ define([], function(){
         if (settings.percentage) {
           return true;
         }
-        return e.data.name !== undefined;
+        return settings.name(e.data) !== undefined;
       })
       .attr('class', 'label');
     label_boxes = labels
@@ -154,9 +155,10 @@ define([], function(){
     label_texts = labels
       .append('text').text(function (e) {
         var percentage = (((e.endAngle - e.startAngle) / (2 * Math.PI)) * 100).toFixed(2),
-          label = [];
-        if (e.data.name !== undefined) {
-          label.push(e.data.name);
+            label = [],
+            name = settings.name(e.data);
+        if (name !== undefined) {
+          label.push(name);
         }
         if (settings.value) {
             label.push(' - ' + e.data.value);
